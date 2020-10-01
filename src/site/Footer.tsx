3fceffa457c2cdbe1262244e7b4affb5c95103a4
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-10-01 17:28:26 -0400

    Started wotking on user endpoints. Changes is App, Auth. Added a Footer

---
+++ src/site/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import {Row} from "reactstrap";
+
+const Footer = () => {
+    return(
+        <footer>
+            {/* <row> */}
+                <p>&copy; Summer J. Kerekes 2020</p>
+            {/* </row> */}
+        </footer>
+    )
+}
+export default Footer;
---
+++ src/site/Header.tsx
@@ -0,0 +1,12 @@
+import React,{FunctionComponent} from "react";
+import { Link } from "react-router-dom";
+
+
+ const Home: FunctionComponent = () => {
+  return (
+      <div>Welcome</div>
+
+  );
+};
+
+export default Home;
