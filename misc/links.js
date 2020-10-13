b349c695c548e170ed3a3cc2a77ef85ad424e3f8
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-10-13 11:29:30 -0400

    add debug file

---
+++ misc/links.js
@@ -0,0 +1,82 @@
+<div className="links-list-styling">
+  <ul className="links-list list-unstyled">
+    <li>
+      <Link to="/home"></Link>
+    </li>
+    <li>
+      <Link to="/auth">Auth</Link>
+    </li>
+    <li>
+      <Link to="/admin/home">Admin Home</Link>
+    </li>  
+    <li>
+      <Link to="/admin/vendors">Vendor</Link>
+    </li>
+    <li>
+      <Link to="/admin/vendorCreate">Vendor Create</Link>
+    </li>
+    <li>
+      <Link to="/admin/vendorList">Vendor List</Link>
+    </li>
+    <li>
+      <Link to="/admin/vendorEdit">Vendor Edit</Link>
+    </li>
+    <li>
+      <Link to="/admin/locations">Locations</Link>
+    </li>
+    <li>
+      <Link to="/admin/locationCreate">Location Create</Link>
+    </li>
+    <li>
+      <Link to="/admin/locationList">Location List</Link>
+    </li>
+    <li>
+      <Link to="/admin/locationEdit">Location Edit</Link>
+    </li>
+    <li>
+      <Link to="/admin/userMgmt">User Management</Link>
+    </li>
+    <li>
+      <Link to="/admin/userTable">User Table</Link>
+    </li>
+    <li>
+      <Link to="/admin/edit">Edit an Acct</Link>
+    </li>
+    <li>
+      <Link to="/admin/grocery">Grocery</Link>
+    </li>
+    <li>
+      <Link to="/admin/groceryCreate">Grocery Create</Link>
+    </li>
+    <li>
+      <Link to="/admin/GroceryList">Grocery List</Link>
+    </li>
+    <li>
+      <Link to="/admin/groceryEdit">Admin Grocery Item Edit </Link>
+    </li>
+    <li>
+      <Link to="/user/home"> User Home</Link>
+    </li>
+    <li>
+      <Link to="/user/vendorList">Vendor List</Link>
+    </li>
+    <li>
+      <Link to="/user/locationList">Location List</Link>
+    </li>
+    <li>
+      <Link to="/user/edit">Edit my Acct</Link>
+    </li>
+    <li>
+      <Link to="/user/grocery">Grocery</Link>
+    </li>
+    <li>
+      <Link to="/user/groceryCreate">Grocery Create</Link>
+    </li>
+    <li>
+      <Link to="/user/groceryList">Grocery List </Link>
+    </li>
+    <li>
+      <Link to="/user/groceryEdit">Grocery Item Edit </Link>
+    </li>
+  </ul>
+</div>;
